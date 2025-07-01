5bf1b77fc9c3c3c861d5c154762306f9a84536c2
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 11:15:03 +0300

    no commit message given

---
+++ clean_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eu
+
+# this script removes all configuration so that you could start over
+
+rm -rf "${HOME}/.eclipse"
+rm -rf "${HOME}/.stm32cubeide" "${HOME}/.stm32cubemx" "${HOME}/.stmcube" "${HOME}/.stmcufinder"
+rm -rf "${HOME}/.streamlit"
