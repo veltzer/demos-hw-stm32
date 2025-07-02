d8d2c8e778a621b7d07eb79b572976284c939836
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-02 03:20:40 +0300

    no commit message given

---
+++ workspace/blinky-cmake/Release/CMakeFiles/blinky-cmake.dir/Sources/main.c.obj.d
@@ -0,0 +1,9 @@
+CMakeFiles/blinky-cmake.dir/Sources/main.c.obj: \
+ /home/mark/git/veltzer/demos-hw-stm32/workspace/blinky-cmake/Sources/main.c \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/lib/gcc/arm-none-eabi/13.3.1/include/stdint.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/stdint.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/machine/_default_types.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/sys/features.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/_newlib_version.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/sys/_intsup.h \
+ /opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.13.3.rel1.linux64_1.0.0.202410170706/tools/arm-none-eabi/include/sys/_stdint.h
