fe8e0fa7ac82cbc971a23ffbeeb1a09e9a236284
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-09 07:57:05 +0300

    no commit message given

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 CLI="/opt/st/stm32cubeide_1.18.1/plugins/com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.linux64_2.2.100.202412061334/tools/bin/STM32_Programmer_CLI"
 
 "${CLI}" -c port=SWD mode=UR -d "blink2/CM4/Release/blink_CM4.elf" --start
+
+# another option is:
+# st-flash write your_firmware.bin 0x08000000
